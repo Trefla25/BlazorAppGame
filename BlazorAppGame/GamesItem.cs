8585c351561d2da3a731faacc72f51d22387eae3
lunguleasa eugen <11alexandru12@gmail.com>
2024-03-22 19:26:02 +0200

    Add project files.

---
+++ BlazorAppGame/GamesItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppGame
+{
+    public class GamesItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+
+    }
+}
+
+
